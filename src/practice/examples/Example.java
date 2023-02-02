076102913f358038bdd68fa92f4228cc8ce206cb
olowookere temitope <olowookeretemitope92@gmail.com>
2023-02-02 22:16:03 +0100

    examples

--- src/practice/examples/Example.java
+++ src/practice/examples/Example.java
@@ -2,6 +2,24 @@ package practice.examples;
 
 
 public class Example {
+   public static void main(String[] args) {
+//        int x = 2;
+//        int y = x * 3;
+//        int z =  y/2;
+//        x = (2+z) %2;
+//        System.out.println(x);
+//
+//        int a = 3 ;
+//        int b = 1 ;
+//        int c = 2 + a * 5 - b ;
+//        System.out.println(c);
+
+        int myFunction(int x, int y) {
+
+        }
+
+
+    }
 
 
 }
